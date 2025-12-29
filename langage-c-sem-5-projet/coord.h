c393d5d6f6acf1a19a42537eec218217f8e423a0
Elena CORTINAS GARCIA <elena@cortinas.fr>
2025-12-29 12:42:04 +0100

    debut coord_h
    Added header file for coordinate structure definition.

---
+++ langage-c-sem-5-projet/coord.h
@@ -0,0 +1,13 @@
+#ifndef COORD_H
+#define COORD_H
+
+#include <stdbool.h>
+
+// Type représentant un point du plan en coordonnées carthésiennes entières
+typedef struct {
+    int x; // abscisse
+    int y; // ordonnée
+} coord_t;
+
+// creer_cord
+// Précondition : aucune
